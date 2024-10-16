0e7bba40b49f78e92e928c6572cbcec0362ea334
syeda <sbtshakeel61@gmail.com>
2024-10-16 19:11:24 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
    <>
    <TypewriterEffectSmoothDemo/>
-   <div className="flex justify-end mr-6 text-white/90 pb-1"><h1>Landing page design by sabahat 2024</h1></div>
+   <div className="flex justify-end mr-6 text-white/90 "><h1>Landing page design by sabahat 2024</h1></div>
    </>
   );
 }
